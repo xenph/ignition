1c333fe9b4838f95c19c10e1f70edf0a99162ddc
maetl <me@maetl.net>
2012-04-25 21:09:01 +1000

    add order by scope for results in descending order

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   has_many :comments
+  scope :desc, order('created_at DESC')
+  
   def self.record project, user, description
     event = Event.new
     event.project = project
